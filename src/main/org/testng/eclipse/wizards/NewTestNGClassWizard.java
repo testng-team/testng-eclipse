3d391f47679a46eccae3c3b249821cffd4bb794c
cbeust <cbeust@990de68d-e119-0410-9bad-49ecb96dc9d4>
2010-01-04 19:54:12 +0000

    Added: New file wizard: can now create a class with annotations, including @DataProvider

---
+++ src/main/org/testng/eclipse/wizards/NewTestNGClassWizard.java
@@ -0,0 +1,191 @@
+package org.testng.eclipse.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.core.runtime.*;
+import org.eclipse.jface.operation.*;
+import java.lang.reflect.InvocationTargetException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.CoreException;
+import java.io.*;
+import org.eclipse.ui.*;
+import org.eclipse.ui.ide.IDE;
+
+/**
+ * This is a sample new wizard. Its role is to create a new file 
+ * resource in the provided container. If the container resource
+ * (a folder or a project) is selected in the workspace 
+ * when the wizard is opened, it will accept it as the target
+ * container. The wizard creates one file with the extension
+ * "java". If a sample multi-page editor (also available
+ * as a template) is registered for the same extension, it will
+ * be able to open it.
+ */
+public class NewTestNGClassWizard extends Wizard implements INewWizard {
+	private NewTestNGClassWizardPage m_page;
+	private ISelection m_selection;
+
+	/**
+	 * Constructor for NewTestNGClassWizard.
+	 */
+	public NewTestNGClassWizard() {
+		super();
+		setNeedsProgressMonitor(true);
+	}
+	
+	/**
+	 * Adding the page to the wizard.
+	 */
+
+	public void addPages() {
+		m_page = new NewTestNGClassWizardPage(m_selection);
+		addPage(m_page);
+	}
+
+	/**
+	 * This method is called when 'Finish' button is pressed in
+	 * the wizard. We will create an operation and run it
+	 * using wizard as execution context.
+	 */
+	public boolean performFinish() {
+		final String containerName = m_page.getSourceFolder();
+		final String fileName = m_page.getClassName() + ".java";
+    try {
+      doFinish(containerName, fileName, new NullProgressMonitor());
+    } catch (CoreException e) {
+      e.printStackTrace();
+    }
+//		IRunnableWithProgress op = new IRunnableWithProgress() {
+//			public void run(IProgressMonitor monitor) throws InvocationTargetException {
+//				try {
+//					doFinish(containerName, fileName, monitor);
+//				} catch (CoreException e) {
+//					throw new InvocationTargetException(e);
+//				} finally {
+//					monitor.done();
+//				}
+//			}
+//		};
+//		try {
+//			getContainer().run(true /* fork */, false /* cancelable */, op);
+//		} catch (InterruptedException e) {
+//			return false;
+//		} catch (InvocationTargetException e) {
+//			Throwable realException = e.getTargetException();
+//			MessageDialog.openError(getShell(), "Error", realException.getMessage());
+//			return false;
+//		}
+		return true;
+	}
+	
+	/**
+	 * The worker method. It will find the container, create the
+	 * file if missing or just replace its contents, and open
+	 * the editor on the newly created file.
+	 */
+	private void doFinish(String containerName, String fileName, IProgressMonitor monitor)
+		  throws CoreException {
+		// create a sample file
+		monitor.beginTask("Creating " + fileName, 2);
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IResource resource = root.findMember(new Path(containerName));
+		if (!resource.exists() || !(resource instanceof IContainer)) {
+			throwCoreException("Container \"" + containerName + "\" does not exist.");
+		}
+		IContainer container = (IContainer) resource;
+		final IFile file = container.getFile(new Path(fileName));
+		try {
+			InputStream stream = openContentStream();
+			if (file.exists()) {
+				file.setContents(stream, true, true, monitor);
+			} else {
+				file.create(stream, true, monitor);
+			}
+			stream.close();
+		} catch (IOException e) {
+		}
+		monitor.worked(1);
+		monitor.setTaskName("Opening file for editing...");
+		getShell().getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				IWorkbenchPage page =
+					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				try {
+					IDE.openEditor(page, file, true);
+				} catch (PartInitException e) {
+				}
+			}
+		});
+		monitor.worked(1);
+	}
+	
+	/**
+	 * We will initialize file contents with a sample text.
+	 */
+
+	private InputStream openContentStream() {
+	  StringBuilder imports = new StringBuilder("import org.testng.annotations.Test;\n");
+	  StringBuilder methods = new StringBuilder();
+	  String dataProvider = "";
+	  String signature = "()";
+
+	  for (String a : NewTestNGClassWizardPage.ANNOTATIONS) {
+	    if (!"".equals(a) && m_page.containsAnnotation(a)) {
+	      imports.append("import org.testng.annotations." + a + ";\n");
+	      if ("DataProvider".equals(a)) {
+	        dataProvider = "(dataProvider = \"dp\")";
+	        methods.append("  @DataProvider\n"
+	            + "  public Object[][] dp() {\n"
+	            + "    return new Object[][] {\n"
+	            + "      new Object[] { 1, \"a\" },\n"
+              + "      new Object[] { 2, \"b\" },\n"
+              + "    };\n"
+              + "  }\n\n"
+              );
+              ;
+            signature = "(Integer n, String s)";
+	      } else {
+  	      methods.append("  @" + a  + "\n"
+  	          + "  public void " + toMethod(a) + "() {\n"
+  	          + "  }\n\n"
+  	          );
+	      }
+	    }
+	  }
+	  String contents =
+	      "package " + m_page.getPackage() + ";\n\n"
+	      + imports
+	      + "\n"
+	      + "public class NewTest {\n"
+	      + "  @Test" + dataProvider + "\n"
+	      + "  public void f" + signature + " {\n"
+	      + "  }\n\n"
+	      + methods
+	      + "}\n"
+	      ;
+		return new ByteArrayInputStream(contents.getBytes());
+	}
+
+  private String toMethod(String a) {
+    return Character.toLowerCase(a.charAt(0)) + a.substring(1);
+  }
+
+  private void throwCoreException(String message) throws CoreException {
+		IStatus status =
+			new Status(IStatus.ERROR, "org.testng.eclipse", IStatus.OK, message, null);
+		throw new CoreException(status);
+	}
+
+	/**
+	 * We will accept the selection in the workbench to see if
+	 * we can initialize from it.
+	 * @see IWorkbenchWizard#init(IWorkbench, IStructuredSelection)
+	 */
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		this.m_selection = selection;
+	}
+}
---
+++ src/main/org/testng/eclipse/wizards/NewTestNGClassWizardPage.java
@@ -0,0 +1,322 @@
+package org.testng.eclipse.wizards;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.dialogs.IDialogPage;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.ContainerSelectionDialog;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * The "New" wizard page allows setting the container for the new file as well
+ * as the file name. The page will only accept file name without the extension
+ * OR with the extension that matches the expected one (java).
+ */
+public class NewTestNGClassWizardPage extends WizardPage {
+  private ISelection m_selection;
+  private Text m_sourceFolderText;
+  private Text m_packageNameText;
+  private Text m_classNameText;
+
+  private Map<String, Button> m_annotations = new HashMap<String, Button>();
+  public static final String[] ANNOTATIONS = new String[] {
+    "BeforeMethod", "AfterMethod", "DataProvider",
+    "BeforeClass", "AfterClass", "",
+    "BeforeTest",  "AfterTest", "",
+    "BeforeSuite", "AfterSuite", ""
+  };
+
+  /**
+   * Constructor for SampleNewWizardPage.
+   * 
+   * @param pageName
+   */
+  public NewTestNGClassWizardPage(ISelection selection) {
+    super("TestNG class");
+    setTitle("TestNG class");
+    setDescription("This wizard creates a new TestNG class.");
+    m_selection = selection;
+  }
+
+  /**
+   * @see IDialogPage#createControl(Composite)
+   */
+  public void createControl(Composite parent) {
+    Composite container = new Composite(parent, SWT.NULL);
+    GridLayout layout = new GridLayout();
+    layout.numColumns = 1;
+    container.setLayout(layout);
+
+    createTop(container);
+    createBottom(container);
+
+    initialize();
+    dialogChanged();
+    setControl(container);
+  }
+
+  private void createBottom(Composite parent) {
+    //
+    // Annotations
+    //
+    {
+      Group g = new Group(parent, SWT.SHADOW_ETCHED_OUT);
+      g.setText("Annotations");
+      GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+      g.setLayoutData(gd);
+
+      GridLayout layout = new GridLayout();
+      g.setLayout(layout);
+      layout.numColumns = 3;
+
+      for (String label : ANNOTATIONS) {
+        if ("".equals(label)) {
+          new Label(g, SWT.NONE);
+        } else {
+          Button b = new Button(g, "".equals(label) ? SWT.None : SWT.CHECK);
+          m_annotations.put(label, b);
+          b.setText("@" + label);
+        }
+      }
+    }
+  }
+
+  private void createTop(Composite parent) {
+    Composite container = new Composite(parent, SWT.NULL);
+    {
+      GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+      container.setLayoutData(gd);
+      GridLayout layout = new GridLayout();
+      layout.numColumns = 3;
+      container.setLayout(layout);
+    }
+
+    //
+    // Source folder
+    //
+    {
+      Label label = new Label(container, SWT.NULL);
+      label.setText("&Source folder:");
+      m_sourceFolderText = new Text(container, SWT.BORDER | SWT.SINGLE);
+      GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+      m_sourceFolderText.setLayoutData(gd);
+      m_sourceFolderText.addModifyListener(new ModifyListener() {
+        public void modifyText(ModifyEvent e) {
+          dialogChanged();
+        }
+      });
+      Button button = new Button(container, SWT.PUSH);
+      button.setText("Browse...");
+      button.addSelectionListener(new SelectionAdapter() {
+        public void widgetSelected(SelectionEvent e) {
+          handleBrowse();
+        }
+      });
+    }
+
+    //
+    // Package name
+    //
+    {
+      Label label = new Label(container, SWT.NULL);
+      label.setText("&Package name:");
+      m_packageNameText = new Text(container, SWT.BORDER | SWT.SINGLE);
+      m_packageNameText.addModifyListener(new ModifyListener() {
+        public void modifyText(ModifyEvent e) {
+          dialogChanged();
+        }
+      });
+      GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+      m_packageNameText.setLayoutData(gd);
+      Button button = new Button(container, SWT.PUSH);
+      button.setText("Browse...");
+      button.addSelectionListener(new SelectionAdapter() {
+        public void widgetSelected(SelectionEvent e) {
+          handleBrowsePackages();
+        }
+      });
+    }
+
+    //
+    // Class name
+    //
+    {
+      Label label = new Label(container, SWT.NULL);
+      label.setText("&Class name:");
+      m_classNameText = new Text(container, SWT.BORDER | SWT.SINGLE);
+      GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+      m_classNameText.setLayoutData(gd);
+      m_classNameText.addModifyListener(new ModifyListener() {
+        public void modifyText(ModifyEvent e) {
+          dialogChanged();
+        }
+      });
+    }
+  }
+
+//  public void _createControl(Composite parent) {
+//    Composite container = new Composite(parent, SWT.NULL);
+//    GridLayout layout = new GridLayout();
+//    container.setLayout(layout);
+//    layout.numColumns = 3;
+//    layout.verticalSpacing = 9;
+//    Label label = new Label(container, SWT.NULL);
+//    label.setText("&Class name:");
+//
+//    m_containerText = new Text(container, SWT.BORDER | SWT.SINGLE);
+//    GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+//    m_containerText.setLayoutData(gd);
+//    m_containerText.addModifyListener(new ModifyListener() {
+//      public void modifyText(ModifyEvent e) {
+//        dialogChanged();
+//      }
+//    });
+//
+//    Button button = new Button(container, SWT.PUSH);
+//    button.setText("Browse...");
+//    button.addSelectionListener(new SelectionAdapter() {
+//      public void widgetSelected(SelectionEvent e) {
+//        handleBrowse();
+//      }
+//    });
+//    label = new Label(container, SWT.NULL);
+//    label.setText("&File name:");
+//
+//    m_fileText = new Text(container, SWT.BORDER | SWT.SINGLE);
+//    gd = new GridData(GridData.FILL_HORIZONTAL);
+//    m_fileText.setLayoutData(gd);
+//    m_fileText.addModifyListener(new ModifyListener() {
+//      public void modifyText(ModifyEvent e) {
+//        dialogChanged();
+//      }
+//    });
+//    initialize();
+//    dialogChanged();
+//    setControl(container);
+//  }
+
+  /**
+   * Tests if the current workbench selection is a suitable container to use.
+   */
+  private void initialize() {
+    if (m_selection != null && m_selection.isEmpty() == false
+        && m_selection instanceof IStructuredSelection) {
+      IStructuredSelection ssel = (IStructuredSelection) m_selection;
+      if (ssel.size() > 1)
+        return;
+      Object obj = ssel.getFirstElement();
+      if (obj instanceof IResource) {
+        IContainer container;
+        if (obj instanceof IContainer) {
+          container = (IContainer) obj;
+        } else {
+          container = ((IResource) obj).getParent();
+        }
+        m_sourceFolderText.setText(container.getFullPath().toString());
+      }
+    }
+    m_classNameText.setText("NewTest");
+  }
+
+  private void handleBrowsePackages() {
+  }
+
+  /**
+   * Uses the standard container selection dialog to choose the new value for
+   * the container field.
+   */
+  private void handleBrowse() {
+    ContainerSelectionDialog dialog = new ContainerSelectionDialog(getShell(), ResourcesPlugin
+        .getWorkspace().getRoot(), false, "Select new file container");
+    if (dialog.open() == ContainerSelectionDialog.OK) {
+      Object[] result = dialog.getResult();
+      if (result.length == 1) {
+        m_packageNameText.setText(((Path) result[0]).toString());
+      }
+    }
+  }
+
+  /**
+   * Ensures that both text fields are set.
+   */
+  private void dialogChanged() {
+    IResource container = ResourcesPlugin.getWorkspace().getRoot().findMember(
+        new Path(getSourceFolder()));
+    String className = getClassName();
+
+    if (container == null || (container.getType() & (IResource.PROJECT | IResource.FOLDER)) == 0) {
+      updateStatus("The source directory must exist");
+      return;
+    }
+    if (getPackageName().length() == 0) {
+      updateStatus("The package must be specified");
+      return;
+    }
+    if (!container.isAccessible()) {
+      updateStatus("Project must be writable");
+      return;
+    }
+    if (className.length() == 0) {
+      updateStatus("Class name must be specified");
+      return;
+    }
+    if (className.replace('\\', '/').indexOf('/', 1) > 0) {
+      updateStatus("Class name must be valid");
+      return;
+    }
+
+    int dotLoc = className.lastIndexOf('.');
+    if (dotLoc != -1) {
+      String ext = className.substring(dotLoc + 1);
+      if (ext.equalsIgnoreCase("java") == false) {
+        updateStatus("File extension must be \"java\"");
+        return;
+      }
+    }
+    updateStatus(null);
+  }
+
+  private void updateStatus(String message) {
+    setErrorMessage(message);
+    setPageComplete(message == null);
+  }
+
+  public String getSourceFolder() {
+    return m_sourceFolderText.getText();
+  }
+
+  public String getPackageName() {
+    return m_packageNameText.getText();
+  }
+
+  public String getClassName() {
+    return m_classNameText.getText();
+  }
+
+  public boolean containsAnnotation(String annotation) {
+    Button b = m_annotations.get(annotation);
+    return b.getSelection();
+  }
+
+  public String getPackage() {
+    return m_packageNameText.getText();
+  }
+}
