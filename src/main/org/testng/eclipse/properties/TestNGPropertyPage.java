0f0c7e4ba272bfca9c1c535050f609e1bd0cfd8c
Asiel Brumfield <asielb@gmail.com>
2010-09-29 15:05:31 -0600

    Updated property page to match functionality of the preferences page.

--- src/main/org/testng/eclipse/properties/TestNGPropertyPage.java
+++ src/main/org/testng/eclipse/properties/TestNGPropertyPage.java
@@ -1,6 +1,7 @@
 package org.testng.eclipse.properties;
 
 
+import java.io.File;
 import java.util.ArrayList;
 
 import org.eclipse.core.resources.IContainer;
@@ -11,9 +12,13 @@ import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.debug.internal.ui.actions.StatusInfo;
+import org.eclipse.debug.internal.ui.preferences.BooleanFieldEditor2;
 import org.eclipse.jdt.internal.ui.wizards.TypedViewerFilter;
 import org.eclipse.jdt.internal.ui.wizards.buildpaths.FolderSelectionDialog;
+import org.eclipse.jface.preference.DirectoryFieldEditor;
+import org.eclipse.jface.preference.FileFieldEditor;
 import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.preference.StringButtonFieldEditor;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
@@ -37,22 +42,24 @@ import org.eclipse.ui.dialogs.PropertyPage;
 import org.eclipse.ui.model.WorkbenchLabelProvider;
 import org.eclipse.ui.views.navigator.ResourceSorter;
 import org.testng.eclipse.TestNGPlugin;
+import org.testng.eclipse.TestNGPluginConstants;
+
 import org.testng.eclipse.util.JDTUtil;
 import org.testng.eclipse.util.PreferenceStoreUtil;
 import org.testng.eclipse.util.SWTUtil;
 
 public class TestNGPropertyPage extends PropertyPage {
   private static final IStatus ERROR = new StatusInfo(IStatus.ERROR, "");
-  private static final String PATH_TITLE = "Path:";
-  private Text m_outdirPath;
-  private Text m_reportersText;
-  private Button m_disabledListenersCheckbox;
+  private static final String PATH_TITLE = "Output directory:";
+  private Text m_outputdir;
+  private Button m_absolutePath;
+  private Button m_disabledDefaultListeners;
+  private Text m_xmlTemplateFile;
   private IProject m_workingProject;
-  private Button m_projectJar;
-  private Button m_fullPathOutput;
+//  private Button m_projectJar;
 
   public void createControl(Composite parent) {
-    setDescription("General TestNG settings:");
+    setDescription("Project TestNG settings:");
     super.createControl(parent);
   }
 
@@ -60,107 +67,85 @@ public class TestNGPropertyPage extends PropertyPage {
    * @see PreferencePage#createContents(Composite)
    */
   protected Control createContents(Composite parent) {
-    Composite composite = new Composite(parent, SWT.NONE);
-    composite.setLayout(new GridLayout(1, false));
-    composite.setLayoutData(new GridData(GridData.FILL | GridData.GRAB_HORIZONTAL));
+    Composite parentComposite = new Composite(parent, SWT.NONE);
+    parentComposite.setLayout(new GridLayout(1, false));
+    parentComposite.setLayoutData(new GridData(GridData.FILL | GridData.GRAB_HORIZONTAL));
 
-    Composite pathComposite= new Composite(composite, SWT.BORDER);
-    pathComposite.setLayout(new GridLayout(4, false));
-    pathComposite.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 4, 1));
     
-    //Label for path field
-    Label pathLabel = new Label(pathComposite, SWT.NONE);
+
+    // Output dir 
+    Label pathLabel = new Label(parentComposite, SWT.NONE);
     pathLabel.setText(PATH_TITLE);
+    m_outputdir = new Text(parentComposite, SWT.SINGLE | SWT.BORDER);
+    m_outputdir.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
-    // Owner text field
-    m_outdirPath = new Text(pathComposite, SWT.SINGLE | SWT.BORDER);
-    m_outdirPath.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-
-    Button browse = new Button(pathComposite, SWT.PUSH);
-    browse.setText("Project"); //$NON-NLS-1$
-    SWTUtil.setButtonGridData(browse);
-    browse.addSelectionListener(new SelectionAdapter() {
-        public void widgetSelected(SelectionEvent e) {
-          handleBrowseAction();
-        }
-      });
-
-    Button browseFS= new Button(pathComposite, SWT.PUSH);
+    
+    // Browse for output dir button
+    Button browseFS= new Button(parentComposite, SWT.PUSH);
     browseFS.setText("Browse"); //$NON-NLS-1$
     SWTUtil.setButtonGridData(browseFS);
     browseFS.addSelectionListener(new SelectionAdapter() {
       public void widgetSelected(SelectionEvent evt) {
-        DirectoryDialog dlg= new DirectoryDialog(m_outdirPath.getShell());
+        DirectoryDialog dlg= new DirectoryDialog(m_outputdir.getShell());
         dlg.setMessage("Select TestNG output directory");
         String selectedDir= dlg.open();
-        m_outdirPath.setText(selectedDir != null ? selectedDir : "");
-        m_fullPathOutput.setSelection(true);
+        m_outputdir.setText(selectedDir != null ? selectedDir : "");
+        m_absolutePath.setSelection(true);
       }
     });
     
-    m_fullPathOutput= new Button(pathComposite, SWT.CHECK);
-    m_fullPathOutput.setText("Absolute path output directory");
-    m_fullPathOutput.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false, 4, 1));
+    m_absolutePath = new Button(parentComposite, SWT.CHECK);
+    m_absolutePath.setText("Absolute output path");
+    m_absolutePath.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false, 4, 1));    
+
+    m_disabledDefaultListeners = new Button(parentComposite, SWT.CHECK);
+    m_disabledDefaultListeners.setText("Disable default listeners");
+    m_disabledDefaultListeners.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false, 4, 1));
+
+    // Template XML
+    Label templateXmlLabel = new Label(parentComposite, SWT.NONE);
+    templateXmlLabel.setText("Template XML file:");
+    m_xmlTemplateFile = new Text(parentComposite, SWT.SINGLE | SWT.BORDER);
+    m_xmlTemplateFile.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+        
     
-    Label pathDetailsLabel = new Label(pathComposite, SWT.WRAP);
-    pathDetailsLabel.setText("Set the directory for the tests output. "
-        + "This location is used by TestNG to generate "
-        + "the output artifacts (including testng-failed.xml).");
-    pathDetailsLabel.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false, 4, 1));
+    // Browse for template XML button
+    Button browseXML = new Button(parentComposite, SWT.PUSH);
+    browseXML.setText("Browse"); //$NON-NLS-1$
+    SWTUtil.setButtonGridData(browseXML);
+    browseXML.addSelectionListener(new SelectionAdapter() {
+      public void widgetSelected(SelectionEvent evt) {
+        DirectoryDialog dlg= new DirectoryDialog(m_xmlTemplateFile.getShell());
+        dlg.setMessage("Select Template XML file");
+        String selectedDir= dlg.open();
+        m_xmlTemplateFile.setText(selectedDir != null ? selectedDir : "");
+      }
+    });
+
+//    m_projectJar= new Button(jarsComposite, SWT.CHECK);
+//    m_projectJar.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING, SWT.NONE, false, false, 2, 1));
     
-    Composite listenersComposite= new Composite(composite, SWT.BORDER);
-    listenersComposite.setLayout(new GridLayout(3, false));
-    listenersComposite.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 3, 1));
-
-    Label disableListener = new Label(listenersComposite, SWT.NONE);
-    disableListener.setText("Disable default listeners:");
-
-    m_disabledListenersCheckbox= new Button(listenersComposite, SWT.CHECK);
-    GridData gd= new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING);
-    gd.horizontalSpan= 2;
-    m_disabledListenersCheckbox.setLayoutData(gd);
-
-    //Label for path field
-    Label listenersLabel = new Label(listenersComposite, SWT.NONE);
-    listenersLabel.setText("Listeners/Reporters:");
-
-    // Owner text field
-    m_reportersText = new Text(listenersComposite, SWT.SINGLE | SWT.BORDER);
-    m_reportersText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-
-    Label listenersDetailsLabel = new Label(listenersComposite, SWT.WRAP);
-    listenersDetailsLabel.setText("Set the additional listeners/reporters (separated by space or ;) to be used when running the TestNG tests.");
-    listenersDetailsLabel.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false, 3, 1));
-
-    Composite jarsComposite= new Composite(composite, SWT.BORDER);
-    jarsComposite.setLayout(new GridLayout(3, false));
-    jarsComposite.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 3, 1));
-    
-    Label jarOrderLabel= new Label(jarsComposite, SWT.NONE);
-    jarOrderLabel.setText("Use project TestNG jar:");
-    
-    m_projectJar= new Button(jarsComposite, SWT.CHECK);
-    m_projectJar.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING, SWT.NONE, false, false, 2, 1));
-    
-    Label projectJarLabel= new Label(jarsComposite, SWT.WRAP | SWT.BOLD);
-    projectJarLabel.setText("The project TestNG jar must be newer than version 5.2 (prior versions are not compatible).");
-    Font normalFont= projectJarLabel.getFont();
-    FontData fd= normalFont.getFontData()[0];
-    fd.setStyle(fd.getStyle() | SWT.ITALIC | SWT.BOLD);
-    projectJarLabel.setFont(new Font(projectJarLabel.getDisplay(), fd));
-    projectJarLabel.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false, 3, 1));
+//    Label projectJarLabel= new Label(jarsComposite, SWT.WRAP | SWT.BOLD);
+//    projectJarLabel.setText("The project TestNG jar must be newer than version 5.2 (prior versions are not compatible).");
+//    Font normalFont= projectJarLabel.getFont();
+//    FontData fd= normalFont.getFontData()[0];
+//    fd.setStyle(fd.getStyle() | SWT.ITALIC | SWT.BOLD);
+//    projectJarLabel.setFont(new Font(projectJarLabel.getDisplay(), fd));
+//    projectJarLabel.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false, 3, 1));
     
     loadDefaults();
 
-    return composite;
+    return parentComposite;
   }
 
   
   public void dispose() {
-    m_projectJar.dispose();
-    m_reportersText.dispose();
-    m_outdirPath.dispose();
-    m_disabledListenersCheckbox.dispose();
+    //m_projectJar.dispose();
+    m_outputdir.dispose();
+    m_absolutePath.dispose();
+    m_disabledDefaultListeners.dispose();
+    m_xmlTemplateFile.dispose();
+    
     super.dispose();
   }
 
@@ -169,11 +154,11 @@ public class TestNGPropertyPage extends PropertyPage {
 
     // Populate the owner text field with the default value
     PreferenceStoreUtil storage= TestNGPlugin.getPluginPreferenceStore();
-    m_outdirPath.setText(storage.getOutputDir(m_workingProject.getName(), true));
-    m_fullPathOutput.setSelection(storage.isOutputAbsolutePath(m_workingProject.getName(), true));
-//    m_reportersText.setText(storage.getReporters(m_workingProject.getName(), true));
-    m_disabledListenersCheckbox.setSelection(storage.hasDisabledListeners(m_workingProject.getName(), true));
-    m_projectJar.setSelection(storage.getUseProjectJar(m_workingProject.getName()));
+    m_outputdir.setText(storage.getOutputDir(m_workingProject.getName(), true));
+    m_absolutePath.setSelection(storage.isOutputAbsolutePath(m_workingProject.getName(), true));
+    m_disabledDefaultListeners.setSelection(storage.hasDisabledListeners(m_workingProject.getName(), true));
+    m_xmlTemplateFile.setText(storage.getXmlTemplateFile(m_workingProject.getName(), true));
+    //m_projectJar.setSelection(storage.getUseProjectJar(m_workingProject.getName()));
   }
 
   protected void performDefaults() {
@@ -183,10 +168,10 @@ public class TestNGPropertyPage extends PropertyPage {
 
   public boolean performOk() {
     PreferenceStoreUtil storage= TestNGPlugin.getPluginPreferenceStore();
-    storage.storeOutputDir(m_workingProject.getName(), m_outdirPath.getText(), m_fullPathOutput.getSelection());
-//    storage.storeReporters(m_workingProject.getName(), m_reportersText.getText());
-    storage.storeDisabledListeners(m_workingProject.getName(), m_disabledListenersCheckbox.getSelection());
-    storage.storeUseProjectJar(m_workingProject.getName(), m_projectJar.getSelection());
+    storage.storeOutputDir(m_workingProject.getName(), m_outputdir.getText(), m_absolutePath.getSelection());
+    storage.storeDisabledListeners(m_workingProject.getName(), m_disabledDefaultListeners.getSelection());
+    storage.storeXmlTemplateFile(m_workingProject.getName(), m_xmlTemplateFile.getText());
+    //storage.storeUseProjectJar(m_workingProject.getName(), m_projectJar.getSelection());
 
     if(super.performOk()) {
       setMessage("Project preferences are saved", INFORMATION);
@@ -245,9 +230,9 @@ public class TestNGPropertyPage extends PropertyPage {
     dialog.setSorter(new ResourceSorter(ResourceSorter.NAME));
 
     if (dialog.open() == Window.OK) {
-      m_outdirPath.setText(((IContainer) dialog.getFirstResult()).getProjectRelativePath()
+      m_outputdir.setText(((IContainer) dialog.getFirstResult()).getProjectRelativePath()
                            .toPortableString());
-      m_fullPathOutput.setSelection(false);
+      m_absolutePath.setSelection(false);
     }
 
   }
@@ -309,6 +294,7 @@ public class TestNGPropertyPage extends PropertyPage {
      */
     public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
     }
-
+    
   }
+  
 }
