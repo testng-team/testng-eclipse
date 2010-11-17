0936d394bcb87c9943a20d98fd6eb0d263ef05c7
Cedric Beust <cedric@beust.com>
2010-11-17 12:41:37 -0800

    Removed dead code.

--- src/main/org/testng/eclipse/refactoring/ConvertFromJUnitAction.java
+++ src/main/org/testng/eclipse/refactoring/ConvertFromJUnitAction.java
@@ -3,21 +3,15 @@ package org.testng.eclipse.refactoring;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.expressions.EvaluationContext;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ltk.ui.refactoring.RefactoringWizardOpenOperation;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PlatformUI;
 
 public class ConvertFromJUnitAction extends AbstractHandler {
-
-  private ISelection m_selection;
 
   public void dispose() {
   }
@@ -47,24 +41,4 @@ public class ConvertFromJUnitAction extends AbstractHandler {
       // operation was cancelled 
     }
   }
-
-  public void selectionChanged(IAction action, ISelection selection) {
-    m_selection = selection;
-  }
-
-//  protected ConvertFromJUnit3Action(IWorkbenchSite site) {
-//    super(site);
-//  }
-
-//  public void run(IStructuredSelection selection) {
-//    System.out.println("Converting");
-//  }
-//    try {
-//      Assert.isTrue(RefactoringAvailabilityTester.isIntroduceIndirectionAvailable(selection));
-//      Object first= selection.getFirstElement();
-//      Assert.isTrue(first instanceof IMethod);
-//      run((IMethod) first);
-//    } catch (CoreException e) {
-//      ExceptionHandler.handle(e, RefactoringMessages.IntroduceIndirectionAction_dialog_title, RefactoringMessages.IntroduceIndirectionAction_unknown_exception);
-//    }
 }
