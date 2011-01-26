79a15da855f492471b16772f86bf6377c075fb48
Cedric Beust <cedric@beust.com>
2011-01-25 16:06:34 -0800

    Javadoc clean up.

--- src/main/org/testng/eclipse/wizards/NewTestNGClassWizard.java
+++ src/main/org/testng/eclipse/wizards/NewTestNGClassWizard.java
@@ -36,19 +36,15 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * This is a sample new wizard. Its role is to create a new file 
- * resource in the provided container. If the container resource
- * (a folder or a project) is selected in the workspace 
- * when the wizard is opened, it will accept it as the target
- * container. The wizard creates one file with the extension
- * "java". If a sample multi-page editor (also available
- * as a template) is registered for the same extension, it will
- * be able to open it.
+ * The wizard that creates a new TestNG class. This wizard looks at the current
+ * selected class and prefills some of its pages based on the information found
+ * in these classes.
+ *
+ * @author Cedric Beust <cedric@beust.com>
  */
 public class NewTestNGClassWizard extends Wizard implements INewWizard {
 	private NewTestNGClassWizardPage m_page;
   private TestNGMethodWizardPage m_methodPage;
-  private IJavaProject m_project;
 
 	/**
 	 * Constructor for NewTestNGClassWizard.
@@ -67,7 +63,6 @@ public class NewTestNGClassWizard extends Wizard implements INewWizard {
 		if (elements.size() > 0) {
 		  m_methodPage = new TestNGMethodWizardPage(elements);
 		  addPage(m_methodPage);
-		  m_project = elements.get(0).getProject();
 		}
 		m_page = new NewTestNGClassWizardPage();
 		addPage(m_page);
