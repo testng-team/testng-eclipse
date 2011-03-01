678d3e55de834dadf2992999ccf6074b983d0f08
Cedric Beust <cedric@beust.com>
2011-03-01 12:45:27 -0800

    New test cases that should not be converted.

---
+++ src/test/java/org/testng/eclipse/convert/JUnit3Test3.java
@@ -0,0 +1,16 @@
+package org.testng.eclipse.convert;
+
+import junit.framework.TestCase;
+
+/**
+ * This class doesn't have any test, setUp, tearDown method. It should
+ * be excluded from the conversion.
+ *
+ * @author Cedric Beust <cedric@beust.com>
+ *
+ */
+public class JUnit3Test3 extends TestCase {
+
+  public void f() {
+  }
+}
---
+++ src/test/java/org/testng/eclipse/convert/JUnit3Test4.java
@@ -0,0 +1,15 @@
+package org.testng.eclipse.convert;
+
+import junit.framework.TestCase;
+
+/**
+ * This class extends TestSuite. It should be excluded from the conversion.
+ * 
+ * @author Cedric Beust <cedric@beust.com>
+ * 
+ */
+public class JUnit3Test4 extends TestCase {
+
+  public void f() {
+  }
+}
