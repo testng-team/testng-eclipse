e3232024c6cb83cf336b272920763a974e2352fa
Cedric Beust <cedric@beust.com>
2011-03-01 12:42:57 -0800

    Restore tests that was accidentally converted and pushed.

--- src/test/java/org/testng/eclipse/convert/JUnit3Test2.java
+++ src/test/java/org/testng/eclipse/convert/JUnit3Test2.java
@@ -1,14 +1,14 @@
 package org.testng.eclipse.convert;
 
-import org.testng.annotations.Test;
+import junit.framework.Assert;
+
 import org.testng.AssertJUnit;
 
 public class JUnit3Test2 extends MyTestCase {
-  @Test()
   public void test1() {
     AssertJUnit.assertEquals(true, true);
     assertMyStuff(null);
-    AssertJUnit.assertEquals("This is", "123","123");
+    Assert.assertEquals("This is", "123","123");
     System.out.println("Worked");
   }
 
