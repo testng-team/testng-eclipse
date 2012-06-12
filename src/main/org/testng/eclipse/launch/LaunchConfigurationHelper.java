badad3f100042feaab699e9cd885099fda9c2322
Cedric Beust <cedric@beust.com>
2012-06-12 11:24:53 -0700

    Generics.

--- src/main/org/testng/eclipse/launch/LaunchConfigurationHelper.java
+++ src/main/org/testng/eclipse/launch/LaunchConfigurationHelper.java
@@ -1,21 +1,20 @@
 package org.testng.eclipse.launch;
 
-import org.testng.eclipse.ui.util.ConfigurationHelper;
-import org.testng.eclipse.util.JDTUtil;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.jdt.core.IJavaProject;
+import org.testng.collections.Lists;
+import org.testng.collections.Sets;
+import org.testng.eclipse.ui.util.ConfigurationHelper;
+import org.testng.eclipse.util.JDTUtil;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 
 public class LaunchConfigurationHelper {
@@ -25,7 +24,7 @@ public class LaunchConfigurationHelper {
   }
   
   public static IResource[] findTestResources(final ILaunchConfiguration conf) throws CoreException {
-    Set resources = new HashSet();
+    Set<IResource> resources = Sets.newHashSet();
 
     resources.addAll(Arrays.asList(findResources(conf, 
         TestNGLaunchConfigurationConstants.DIRECTORY_TEST_LIST)));
@@ -36,17 +35,17 @@ public class LaunchConfigurationHelper {
     resources.addAll(Arrays.asList(findResources(conf,
         TestNGLaunchConfigurationConstants.SUITE_TEST_LIST)));
     
-    return (IResource[]) resources.toArray(new IResource[resources.size()]);
+    return resources.toArray(new IResource[resources.size()]);
   }
   
   public static IResource[] findResources(final ILaunchConfiguration conf, 
                                    final String key) throws CoreException {
-    List resources = new ArrayList();
-    List containerPaths = conf.getAttribute(key, Collections.EMPTY_LIST);
+    List<IResource> resources = Lists.newArrayList();
+    List<String> containerPaths = conf.getAttribute(key, Collections.EMPTY_LIST);
     IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
     
     for(int i = 0; i < containerPaths.size(); i++) {
-      String resPath = (String) containerPaths.get(i);
+      String resPath = containerPaths.get(i);
       IResource res = root.findMember(resPath);
       
       if(null != res) {
@@ -54,6 +53,6 @@ public class LaunchConfigurationHelper {
       }
     }
     
-    return (IResource[]) resources.toArray(new IResource[resources.size()]);
+    return resources.toArray(new IResource[resources.size()]);
   }
 }
