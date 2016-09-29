bace2dd8dec350cc17d9e5c545667099568e4123
Nick Tan <missedone@gmail.com>
2016-09-29 07:55:08 -0700

    issue #288: DEFAULT_LOG_LEVEL = 2

--- testng-eclipse-plugin/src/main/org/testng/eclipse/launch/TestNGLaunchConfigurationConstants.java
+++ testng-eclipse-plugin/src/main/org/testng/eclipse/launch/TestNGLaunchConfigurationConstants.java
@@ -4,9 +4,8 @@ import org.testng.eclipse.TestNGPlugin;
 
 
 /**
- * Constants used to pass information from the launch manager to the
- * launcher.
-
+ * Constants used to pass information from the launch manager to the launcher.
+ * 
  * @author cbeust
  */
 public abstract class TestNGLaunchConfigurationConstants {
@@ -20,69 +19,60 @@ public abstract class TestNGLaunchConfigurationConstants {
    * Root directory of the tests.  If this property is set,
    * TestNG will run all the tests contained in this directory.
    */
-  public static final String DIRECTORY_TEST_LIST = 
-    make("DIRECTORY_TEST_LIST"); //$NON-NLS-1$
+  public static final String DIRECTORY_TEST_LIST = make("DIRECTORY_TEST_LIST"); //$NON-NLS-1$
   
   /**
    * List of classes
    */
-  public static final String CLASS_TEST_LIST = 
-    make("CLASS_TEST_LIST"); //$NON-NLS-1$
+  public static final String CLASS_TEST_LIST =  make("CLASS_TEST_LIST"); //$NON-NLS-1$
 
   /**
    * List of methods. This is replaced by {@link #ALL_METHODS_LIST}.
    */
-  public static final String METHOD_TEST_LIST = 
-    make("METHOD_TEST_LIST"); //$NON-NLS-1$
+  public static final String METHOD_TEST_LIST = make("METHOD_TEST_LIST"); //$NON-NLS-1$
   
-  public static final String ALL_METHODS_LIST = 
-    make("ALL_CLASS_METHODS"); //$NON-NLS-1$
+  public static final String ALL_METHODS_LIST = make("ALL_CLASS_METHODS"); //$NON-NLS-1$
   
   /**
    * List of packages
    */
-  public static final String PACKAGE_TEST_LIST = 
-    make("PACKAGE_TEST_LIST"); //$NON-NLS-1$
+  public static final String PACKAGE_TEST_LIST = make("PACKAGE_TEST_LIST"); //$NON-NLS-1$
   
   /**
    * List of sources
    */
-  public static final String SOURCE_TEST_LIST = 
-      make("SOURCE_TEST_LIST"); //$NON-NLS-1$
+  public static final String SOURCE_TEST_LIST = make("SOURCE_TEST_LIST"); //$NON-NLS-1$
   
   /**
    * List of groups
    */
-  public static final String GROUP_LIST = 
-    make("GROUP_LIST"); //$NON-NLS-1$
-  
+  public static final String GROUP_LIST = make("GROUP_LIST"); //$NON-NLS-1$
+
   public static final String GROUP_CLASS_LIST = make("GROUP_LIST_CLASS");
-  
+
+  /**
+   * default log level for nothing
+   */
   public static final int DEFAULT_LOG_LEVEL = 2;
   
   /**
    * List of suites
    */
-  public static final String SUITE_TEST_LIST = 
-    make("SUITE_TEST_LIST");  //$NON-NLS-1$
-  
+  public static final String SUITE_TEST_LIST = make("SUITE_TEST_LIST");  //$NON-NLS-1$
+
   /**
    * Port of the launcher
    */
-  public static final String PORT = 
-    make("PORT"); //$NON-NLS-1$
+  public static final String PORT = make("PORT"); //$NON-NLS-1$
   
   /**
    * Name of the project
    */
-  public static final String PROJECT_NAME = 
-    make("PROJECT_NAME"); //$NON-NLS-1$
+  public static final String PROJECT_NAME = make("PROJECT_NAME"); //$NON-NLS-1$
   
-  public static final String TESTNG_RUN_NAME_ATTR = 
-    make("SUBNAME"); //$NON-NLS-1$
+  public static final String TESTNG_RUN_NAME_ATTR = make("SUBNAME"); //$NON-NLS-1$
 
-  public static final String TEMP_SUITE_LIST = 
-    make("TEMP_SUITE_LIST"); //$NON-NLS-1$
+  public static final String TEMP_SUITE_LIST = make("TEMP_SUITE_LIST"); //$NON-NLS-1$
   
   public static final String TYPE = make("TYPE");  //$NON-NLS-1$ 
   
@@ -98,8 +88,8 @@ public abstract class TestNGLaunchConfigurationConstants {
 
   public static final String VM_ENABLEASSERTION_OPTION = "-ea";
 
-  public static final String PRE_DEFINED_LISTENERS=
-      make("PRE_DEFINED_LISTENERS");
+  public static final String PRE_DEFINED_LISTENERS = make("PRE_DEFINED_LISTENERS");
+
   // What kind of run we are doing
   // This would be a nice place for an enum when jdk1.5 or later can be 
   // required.
